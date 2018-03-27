d478e0c5d9436e470934fb4f1a6341d4d6b33d8b
Daniel Longo <DanielLongo@Daniels-MBP-3.hsd1.ca.comcast.net>
2018-03-26 21:13:48 -0700

    updates

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os 
 class CryptoRnn(object):
 	def __init__(self):
-		self.key = "5GC8VYTZ-3LMAY2RT-RGMBKY88-FUBL52D1" #change
-		self.secret = "6b8d51e9e2fdcd24bb1a4482ae6f539bd1fd10b21405b0c3ffb75d2dedf924f4447501f4324c77f2434c572dcd41cb1fdf3037fc91ccf603fa74720264c31cfa"
+		self.key = ""
+		self.secret = ""
 		self.currencyPairs = ["BTC_AMP","BTC_ARDR","BTC_BCN","BTC_BCY","BTC_BELA","BTC_BLK","BTC_BTCD","BTC_BTM","BTC_BTS","BTC_BURST","BTC_CLAM","BTC_DASH","BTC_DCR","BTC_DGB",
 			"BTC_DOGE","BTC_EMC2","BTC_ETC","BTC_ETH","BTC_EXP","BTC_FCT","BTC_FLDC","BTC_FLO","BTC_GAME","BTC_GNO","BTC_GNT","BTC_GRC","BTC_HUC","BTC_LBC",
 			"BTC_LSK","BTC_LTC","BTC_MAID","BTC_NAV","BTC_NEOS","BTC_NMC","BTC_NXC","BTC_NXT","BTC_OMNI","BTC_PASC","BTC_PINK","BTC_POT",
@@ -11,11 +11,19 @@ class CryptoRnn(object):
 			"ETH_ZEC","USDT_BTC","USDT_DASH","USDT_ETC","USDT_ETH","USDT_LTC","USDT_NXT","USDT_REP","USDT_STR","USDT_XMR","USDT_XRP","USDT_ZEC","XMR_BCN",
 			"XMR_BLK","XMR_BTCD","XMR_DASH","XMR_LTC","XMR_MAID","XMR_NXT","XMR_ZEC"]
 		self.dataPath = "/Users/DanielLongo/Desktop/CryptoRnn/cryptoData/"
+
 		self.start = 1521590400
 		self.end = 1521676800
+
 		self.interval = 600 #interval is the time ampunt at which trades are grouped
 		self.PPE = 20 #periods per example
 		self.ExPerBatch = 2 #examples per batch
+		self.sequence_length = 10 # len(time)
+		#RNN Params
+		
+		self.epoch = 10000
+		self.learningRate = .01 
+		self.state_size = 100 # depth of rnn number of hidden layers 
 
 	def checkData(self): #ensures there is data for start and end dates
 		files = os.listdir(self.dataPath)
